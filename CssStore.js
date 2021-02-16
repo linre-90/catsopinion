dbad5396870ca9f03777ffc26d21aff4d8af94f7
linre-90 <juho.lindemark@gmail.com>
2021-02-16 14:02:43 +0200

    css split works

--- CssStore.js
+++ CssStore.js
@@ -1,8 +1,8 @@
-const homeCss = ["main.css"];
-const blogCss = ["main.css", "blog.css"];
-const contactCss = ["main.css", "contact.css"];
-const funzoneCss = ["main.css", "funzone.css"];
-const privacyCss = ["main.css", "privacy.css"];
+const homeCss = ["main.css", "header.css", "footer.css", "navSection.css"];
+const blogCss = ["main.css", "blog.css",  "footer.css", "navSection.css"];
+const contactCss = ["main.css", "contact.css", "header.css", "footer.css", "navSection.css"];
+const funzoneCss = ["main.css", "funzone.css", "header.css", "footer.css", "navSection.css"];
+const privacyCss = ["main.css", "privacy.css", "header.css", "footer.css", "navSection.css"];
 const formsuccesCss = ["main.css"];
 
 module.exports = {homeCss, blogCss, contactCss, funzoneCss, privacyCss, formsuccesCss}
