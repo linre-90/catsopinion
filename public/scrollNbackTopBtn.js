bd789da2d314f833313f510c46ab842e25280e39
linre-90 <juho.lindemark@gmail.com>
2021-02-16 14:07:57 +0200

    removed report bug btn

--- public/scrollNbackTopBtn.js
+++ public/scrollNbackTopBtn.js
@@ -2,11 +2,7 @@
 // plain javascript
 let backToTopBTN = document.getElementById("backToTopBtn");
 
-let reportBugBTN = document.getElementById("reportBugBTN");
-
 const limitToShowBtn = 40;
-
-let reportBugOk = false;
 
 window.onscroll = () =>{
     checkScroll();
@@ -15,11 +11,9 @@ window.onscroll = () =>{
 const checkScroll = () =>{
     if(document.body.scrollTop > limitToShowBtn || document.documentElement.scrollTop > limitToShowBtn){
         backToTopBTN.style.display = "block";
-        reportBugBTN.style.display = "block";
         backToTopBTN.addEventListener("click", backToTop);
     }else{
         backToTopBTN.style.display = "none";
-        reportBugBTN.style.display = "none";
         backToTopBTN.removeEventListener("click",backToTop)
     }
 }
