90174070b51f1f40b61165b4143f575f482b6e7b
linre-90 <juho.lindemark@gmail.com>
2021-02-20 20:03:28 +0200

    fixed firefox cant open blog post error

--- public/Blog.js
+++ public/Blog.js
@@ -7,11 +7,24 @@ const postInject =  document.getElementById("insertpost");
 
 const open = async (event) => {
     let postDataUrl;
-    event.path.forEach(object => {
-        if(object.localName == "button"){
-            postDataUrl = object.id.toString();
-        }
-    });
+    // chrome and edge supports path
+    if(event.path)
+    {
+        event.path.forEach(object => {
+            if(object.localName == "button"){
+                postDataUrl = object.id.toString();
+            }
+        });
+    }
+    // firefox works with composedPath
+    if(event.composedPath && !postDataUrl){
+        event.composedPath().forEach(element => {
+            if(element.nodeName == "BUTTON"){
+                postDataUrl = element.id.toString();
+            }
+        });
+    }
+    
     spinner.style.display = "block";
     backdrop.style.display = "block";
     postView.style.display = "block";
